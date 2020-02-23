7e209be58ed578c62b8e308a82d26c268e469ecc
Eric Xing <ericxhc@yahoo.ca>
2020-02-22 23:40:24 -0500

    Use css styles more. Second iteration

--- focusedSE.js
+++ focusedSE.js
@@ -1,25 +1,9 @@
-var showSidebar = true;
-var content = document.getElementById('content');
-//content.style.paddingLeft='0px';
-content.style.width='calc(100% - 48px)';
-
-var sidebarButton = document.createElement("div");
-sidebarButton.className='sidebarButton';
-sidebarButton.style.minWidth='24px';
-sidebarButton.style.backgroundColor='lightgrey';
-sidebarButton.style.cursor='pointer';
-sidebarButton.addEventListener(
-	'click',
-	()=> {
-		if (showSidebar) {
-			leftSidebar.style.width='0px';
-		} else {
-			leftSidebar.style.width='auto';
-		}
-		showSidebar = !showSidebar;
-	},
-	false)
-
-var leftSidebar = document.getElementById('left-sidebar');
-leftSidebar.style.display = 'inline-flex';
-leftSidebar.append(sidebarButton);
+const bgColor = window.getComputedStyle(document.body).backgroundColor;
+const leftSidebar = document.getElementById('left-sidebar');
+const sidebar = document.getElementById('sidebar');
+if (leftSidebar) {
+	leftSidebar.style.backgroundColor = bgColor;
+}
+if (sidebar) {
+	sidebar.style.backgroundColor = bgColor;
+}
